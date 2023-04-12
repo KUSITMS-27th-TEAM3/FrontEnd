12edb50d43333a807d7a10af32a04d0181bdbce6
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-04-13 00:46:58 +0900

    feat : 초기폴더구조 셋팅

---
+++ src/api/API.tsx
@@ -0,0 +1,68 @@
+import axios from 'axios';
+
+const instance = axios.create();
+
+instance.defaults.withCredentials = true;
+instance.defaults.headers['Content-Type'] = 'application/json';
+
+const errCheck = (err: unknown) => {
+  let message;
+  if (err instanceof Error) message = err.message;
+  else message = String(err);
+  console.error(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${message}`);
+};
+
+const get = async (url: string) => {
+  try {
+    const { data } = await instance.get(url);
+    return data;
+  } catch (error) {
+    if (error instanceof Error)
+      throw new Error(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${error.message}`);
+  }
+};
+
+const del = async (url: string) => {
+  try {
+    const { data } = await instance.delete(url);
+    return data;
+  } catch (error) {
+    if (error instanceof Error)
+      throw new Error(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${error.message}`);
+  }
+};
+
+/**
+ *
+ * @param url : url주소 ex) /api/shops
+ * @param post : body
+ * @param config : default = null, 이미지 보낼때만 "imgPost" 설정 > 'Content-Type': 'multipart/form-data' 설정
+ * @returns : res.data 반환
+ */
+const post = async (url: string, post: {}, config: 'imgPost' | null = null) => {
+  try {
+    if (config === 'imgPost') {
+      const result = await instance.post(url, post, {
+        headers: { 'Content-Type': `multipart/form-data` },
+      });
+      return result.data;
+    }
+    const result = await instance.post(url, post);
+    return result.data;
+  } catch (error) {
+    if (error instanceof Error)
+      throw new Error(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${error.message}`);
+  }
+};
+
+const patch = async (url: string, patch: {}) => {
+  try {
+    const { data } = await instance.patch(url, patch);
+    return data;
+  } catch (error) {
+    if (error instanceof Error)
+      throw new Error(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${error.message}`);
+  }
+};
+
+export { get, del as delete, post, patch };

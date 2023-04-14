586ebbd8fe28a05a34647c774195141b1bc6ca42
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-04-14 13:41:22 +0900

    feat : Errorboundary, Spinner 추가

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,34 @@
+import React, { Component, ErrorInfo, ReactNode } from 'react';
+import { FlexContainer } from '../../../BoBHuB_Refactor/BoBHuB/FE/src/styles/GlobalStyle';
+
+interface Props {
+  children?: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false,
+  };
+
+  public static getDerivedStateFromError(_: Error): State {
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error('Uncaught error:', error, errorInfo);
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return <FlexContainer>ErrorBoundary로 에러처리하기</FlexContainer>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
---
+++ src/components/Spinner.tsx
@@ -0,0 +1,18 @@
+import { ClipLoader } from 'react-spinners';
+import styled from 'styled-components';
+import { FlexContainer } from '../styles/GlobalStyle';
+
+const SpinnerContainer = styled(FlexContainer)`
+  justify-content: center;
+  height: 100vh;
+`;
+
+const Spinner = () => {
+  return (
+    <SpinnerContainer>
+      <ClipLoader color={'#E59A59'} size={70} />
+    </SpinnerContainer>
+  );
+};
+
+export default Spinner;

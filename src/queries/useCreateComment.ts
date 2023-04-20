708e82bd7aff9636703b42995a098a56080dd17f
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-04-20 13:08:59 +0900

    feat : react query 추가

---
+++ src/queries/useCreateComment.ts
@@ -0,0 +1,27 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import * as API from '../api/API';
+
+const postComment = async (comment: PostComment) => {
+  return await API.post('/api/comments', comment);
+};
+
+type UseCreateComment = {
+  onSuccessCb: () => void;
+  onErrorCb: () => void;
+  shopId: number | undefined;
+};
+
+export const useCreateComment = ({ onSuccessCb, onErrorCb, shopId }: UseCreateComment) => {
+  const queryClient = useQueryClient();
+  const mutation = useMutation(postComment, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(['comment', shopId]);
+      onSuccessCb();
+    },
+    onError: () => {
+      onErrorCb();
+    },
+  });
+
+  return { mutation };
+};

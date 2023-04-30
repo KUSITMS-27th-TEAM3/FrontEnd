c830dd758ef15229513f4d3993f26ca84f958d43
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-04-30 23:00:42 +0900

    refactor : Navbar 로직 link로 변경

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const NavBarContainer = styled.nav`
@@ -27,48 +27,32 @@ const NavBarContainer = styled.nav`
   }
 `;
 
+const BasicLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`;
+
 const NavBar = () => {
-  const navigate = useNavigate();
-
-  const handleLogoClick = () => {
-    navigate(`/`);
-  };
-
-  const handleNavClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const currentId = e.currentTarget.id;
-    console.log(currentId);
-
-    if (currentId === 'memory') {
-      navigate(`/memory`);
-    } else if (currentId === 'diary') {
-      navigate('/diary');
-    } else if (currentId === 'funeral') {
-      navigate('/funeral ');
-    } else if (currentId === 'login') {
-      navigate('/login');
-    } else if (currentId === 'mypage') {
-      navigate('/mypage');
-    }
-  };
-
   return (
     <NavBarContainer>
-      <img src="/img/BI.svg" alt="logo" onClick={handleLogoClick} />
-      <button id="memory" onClick={handleNavClick}>
-        추억앨범
-      </button>
-      <button id="diary" onClick={handleNavClick}>
-        의미기록
-      </button>
-      <button id="funeral" onClick={handleNavClick}>
-        장례정보
-      </button>
-      <button id="login" onClick={handleNavClick}>
-        로그인
-      </button>
-      <button id="mypage" onClick={handleNavClick}>
-        마이페이지
-      </button>
+      <BasicLink to="/">
+        <img src="/img/BI.svg" alt="logo" />
+      </BasicLink>
+      <BasicLink to="/memory">
+        <button>추억앨범</button>
+      </BasicLink>
+      <BasicLink to="/diary">
+        <button>의미기록</button>
+      </BasicLink>
+      <BasicLink to="/funeral">
+        <button>장례정보</button>
+      </BasicLink>
+      <BasicLink to="/login">
+        <button>login</button>
+      </BasicLink>
+      <BasicLink to="/mypage">
+        <button>마이페이지</button>
+      </BasicLink>
     </NavBarContainer>
   );
 };

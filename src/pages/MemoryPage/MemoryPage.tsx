d8d1590a88af5f6ed1c9a508f0ebd83b0393598b
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-05-06 22:35:58 +0900

    feat : 공통 layout 컴포넌트 적용

--- src/pages/MemoryPage/MemoryPage.tsx
+++ src/pages/MemoryPage/MemoryPage.tsx
@@ -1,27 +1,8 @@
-import Navbar from '../../components/Navbar';
-import { Outlet } from 'react-router-dom';
-import MemoryNav from './components/MemoryNav';
-import Advertisment from '../../components/Advertisment';
-import styled from 'styled-components';
-import Footer from '../../components/Footer';
-
-const MemoryPageContainer = styled.section`
-  position: relative;
-  &::-webkit-scrollbar {
-    display: none;
-  }
-`;
+import LayoutContainer from '../../components/LayoutContainer';
+import Memory from './Memory';
 
 const MemoryPage = () => {
-  return (
-    <MemoryPageContainer>
-      <Navbar />
-      <Advertisment />
-      <MemoryNav />
-      <Outlet />
-      <Footer />
-    </MemoryPageContainer>
-  );
+  return <LayoutContainer children={<Memory />} />;
 };
 
 export default MemoryPage;

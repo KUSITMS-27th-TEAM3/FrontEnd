ffc958b19135a22668ac1f3647fed7ae7f9b4508
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-05-08 15:08:32 +0900

    feat : 감정태그 조건부 렌더링

--- src/pages/MemoryPage/Memory.tsx
+++ src/pages/MemoryPage/Memory.tsx
@@ -1,13 +1,22 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Advertisment from '../../components/Advertisment';
 import { MemoryNav, EmotionTags } from './components';
+import { useEffect, useState } from 'react';
 
 const Memory = () => {
+  const [currentPath, setCurrentPath] = useState<string>('');
+  const location = useLocation();
+  const { pathname } = location;
+
+  useEffect(() => {
+    setCurrentPath(pathname);
+  }, [location]);
+
   return (
     <>
       <Advertisment />
       <MemoryNav />
-      <EmotionTags />
+      {currentPath !== '/memory/question' && <EmotionTags />}
       <Outlet />
     </>
   );

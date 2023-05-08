6e4e04b2ff1426ca959730cd613d773e33b82e40
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-05-08 14:58:41 +0900

    feat : 감정태그컴포넌트 추가

---
+++ src/pages/MemoryPage/components/EmotionTags.tsx
@@ -0,0 +1,90 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const TagButton = styled.button`
+  all: unset;
+  font-family: ${({ theme }) => theme.font.family.pretendard_medium};
+  border-radius: 50px;
+  background-color: ${({ theme }) => theme.color.grayScale.lightGray};
+  font-size: 16px;
+  padding: 1em 2em;
+  cursor: pointer;
+  &.active {
+    background-color: ${({ theme }) => theme.color.main.orange};
+    color: ${({ theme }) => theme.color.grayScale.white}};
+  }
+`;
+
+const EmotionContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: max(8vw, 100px);
+  width: 80vw;
+  justify-content: space-between;
+
+  @media screen and (max-width: 1500px) {
+    ${TagButton} {
+      font-size: 10px;
+    }
+  }
+`;
+
+type Tag = {
+  name: string;
+  isActive: boolean;
+};
+
+const temp = [
+  { name: '감정태그', isActive: false },
+  { name: '감정태그', isActive: false },
+  { name: '감정태그', isActive: false },
+  { name: '감정태그', isActive: false },
+  { name: '감정태그', isActive: false },
+  { name: '감정태그', isActive: false },
+  { name: '감정태그', isActive: false },
+  { name: '감정태그', isActive: false },
+  { name: '감정태그', isActive: false },
+  { name: '감정태그', isActive: false },
+];
+
+const EmotionTags = () => {
+  const [tags, setTags] = useState<Tag[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const handleTagActive = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const targetIdx = Number(e.currentTarget.id);
+    setTags((current) =>
+      current.map(({ name, isActive }, idx) => {
+        if (targetIdx === idx) {
+          return { name, isActive: !isActive };
+        } else return { name, isActive: false };
+      }),
+    );
+  };
+
+  useEffect(() => {
+    setTags([...temp]);
+    setLoading(false);
+  }, []);
+
+  return (
+    <EmotionContainer>
+      {loading ? (
+        <></>
+      ) : (
+        tags.map(({ name, isActive }, idx) => (
+          <TagButton
+            className={isActive ? 'active' : ''}
+            key={`${name}${idx}`}
+            id={idx.toString()}
+            onClick={handleTagActive}
+          >
+            {name}
+          </TagButton>
+        ))
+      )}
+    </EmotionContainer>
+  );
+};
+
+export default EmotionTags;
---
+++ src/pages/MemoryPage/components/index.tsx
@@ -0,0 +1,3 @@
+export { default as EmotionTags } from './EmotionTags';
+export { default as AlbumPresenter } from './AlbumPresenter';
+export { default as MemoryNav } from './MemoryNav';

700db76eb52f2f894b92779bf1347deb85000e4f
Jieun Lim <jieun776121@gmail.com>
2023-05-13 00:30:35 +0900

    feat : 마이페이지 첨부 이미지

--- src/components/MainPageNavbar.tsx
+++ src/components/MainPageNavbar.tsx
@@ -24,7 +24,7 @@ const NavBarContainer = styled.nav`
   color: white;
 
   position: absolute;
-  z-index: 1;
+  z-index: 2;
   background-color: rgba(255, 255, 255, 0);
   top: 0.6vw;
 
@@ -47,32 +47,32 @@ const NavBarContainer = styled.nav`
 const BIContainer = styled.div``;
 
 const MainPageNavbar = () => {
-    return (
-        <NavBarContainer>
-            <BIContainer>
-                <WhiteLink to="/">
-                    <img src="/img/BI.svg" alt="logo" />
-                </WhiteLink>
-            </BIContainer>
-            <LinkContainer>
-                <WhiteLink to="/memory/sharedAlbum">
-                    <button>추억기록</button>
-                </WhiteLink>
-                <span> | </span>
-                <WhiteLink to="/funeral">
-                    <button>장례정보</button>
-                </WhiteLink>
-                <span> | </span>
-                <WhiteLink to="/login">
-                    <button>로그인</button>
-                </WhiteLink>
-                <span> | </span>
-                <WhiteLink to="/mypage">
-                    <button>마이페이지</button>
-                </WhiteLink>
-            </LinkContainer>
-        </NavBarContainer>
-    );
+  return (
+    <NavBarContainer>
+      <BIContainer>
+        <WhiteLink to="/">
+          <img src="/img/BI.svg" alt="logo" />
+        </WhiteLink>
+      </BIContainer>
+      <LinkContainer>
+        <WhiteLink to="/memory/sharedAlbum">
+          <button>추억기록</button>
+        </WhiteLink>
+        <span> | </span>
+        <WhiteLink to="/funeral">
+          <button>장례정보</button>
+        </WhiteLink>
+        <span> | </span>
+        <WhiteLink to="/login">
+          <button>로그인</button>
+        </WhiteLink>
+        <span> | </span>
+        <WhiteLink to="/mypage">
+          <button>마이페이지</button>
+        </WhiteLink>
+      </LinkContainer>
+    </NavBarContainer>
+  );
 };
 
 export default MainPageNavbar;

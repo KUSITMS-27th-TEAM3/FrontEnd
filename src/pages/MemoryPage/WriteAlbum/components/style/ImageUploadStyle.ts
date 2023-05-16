340c82dce87be811c7b81deca569e262cbbda7a2
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-05-16 17:21:38 +0900

    feat : Image Upload Style코드 분리

---
+++ src/pages/MemoryPage/WriteAlbum/components/style/ImageUploadStyle.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const ImageContainer = styled.div`
+  position: relative;
+  img {
+    width: 100%;
+    height: auto;
+    object-fit: contain;
+  }
+`;
+
+export const ImageUploadBox = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 30%;
+  cursor: pointer;
+  aspect-ratio: 1;
+
+  transform: translate(-50%, -50%);
+  background-color: rgba(255, 255, 255, 0.12);
+  border: 3px dashed rgba(255, 255, 255, 0.56);
+  box-sizing: border-box;
+  color: ${(props) => props.theme.color.grayScale.white};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .imageAlbum {
+    width: 30%;
+    height: 30%;
+  }
+
+  .imageCaption {
+    font-size: 26px;
+    font-family: ${(props) => props.theme.font.family.pretendard_bold};
+    margin-top: 35px;
+  }
+`;

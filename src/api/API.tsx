bd8155315b3bd816a36d5f519c1f9a655fa3c725
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-05-18 19:44:39 +0900

    feat : axios intercepter 공통로직 추가

--- src/api/API.tsx
+++ src/api/API.tsx
@@ -3,16 +3,61 @@ import axios from 'axios';
 const instance = axios.create();
 
 instance.defaults.withCredentials = true;
-instance.defaults.headers['Content-Type'] = 'application/json';
-instance.defaults.headers.common['Authorization'] = localStorage.getItem('Authorization')
+// instance.defaults.headers['Content-Type'] = 'application/json';
+// instance.defaults.headers.common['Authorization'] = localStorage.getItem('Authorization');
 instance.defaults.baseURL = 'http://52.78.181.46';
 
-const errCheck = (err: unknown) => {
-  let message;
-  if (err instanceof Error) message = err.message;
-  else message = String(err);
-  console.error(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${message}`);
+const getAccessToken = () => {
+  return localStorage.getItem('Authorization');
 };
+
+const getRefreshToken = () => {
+  return localStorage.getItem('RefreshToken');
+};
+
+instance.interceptors.request.use(
+  (config) => {
+    const accessToken = getAccessToken();
+
+    config.headers['Content-Type'] = 'application/json';
+    config.headers['Authorization'] = `Bearer ${accessToken}`;
+
+    return config;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  },
+);
+
+// instance.interceptors.response.use(
+//   (response) => {
+//     if (response.status === 404) {
+//       console.log('404 페이지로 넘어가야 함!');
+//     }
+
+//     return response;
+//   },
+//   async (error) => {
+//     if (error.response?.status === 401) {
+//       // isTokenExpired() - 토큰 만료 여부를 확인하는 함수
+//       // tokenRefresh() - 토큰을 갱신해주는 함수
+//       if (isTokenExpired()) await tokenRefresh();
+
+//       const accessToken = getToken();
+
+//       error.config.headers = {
+//         'Content-Type': 'application/json',
+//         Authorization: `Bearer ${accessToken}`,
+//       };
+
+//       // 중단된 요청을(에러난 요청)을 토큰 갱신 후 재요청
+//       const response = await axios.request(error.config);
+//       return response;
+//     }
+//     return Promise.reject(error);
+//   }
+// );
 
 const get = async (url: string) => {
   try {

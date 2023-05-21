361d4e3fca057a7d7985f2bd97fb713d5983c874
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-05-21 16:56:31 +0900

    refactor : 경로단순화

---
+++ src/pages/MemoryDetail/components/index.tsx
@@ -0,0 +1,5 @@
+export { default as CommentList } from './CommentList';
+export { default as InputForm } from './InputForm';
+export { default as ReplyComment } from './ReplyComment';
+export { default as ImageContent } from './ImageContent';
+export { default as TextContent } from './TextContent';

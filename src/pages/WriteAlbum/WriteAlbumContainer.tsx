cc129dcbe05383ceaa21688c4d2b9e0965b3f1f0
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-05-23 01:12:28 +0900

    feat : api연동 핸들러 추가

--- src/pages/WriteAlbum/WriteAlbumContainer.tsx
+++ src/pages/WriteAlbum/WriteAlbumContainer.tsx
@@ -3,28 +3,54 @@ import { FlexContainer, IconButton } from '../../components/CommonStyle';
 import { useEffect, useState } from 'react';
 import { WriteBox } from './components/style/WriteFormStyle';
 import { ContentForm, EmotionForm, RadioForm, TitleForm, ImageUpload } from './components';
-import { isUploadAtom } from '../../atom/atom';
-import { useSetRecoilState } from 'recoil';
+import { activeTagAtom, isUploadAtom } from '../../atom/atom';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { postAlbum } from './WriteAlbumApi';
 
 const WriteAlbumWrapper = styled(FlexContainer)``;
 
 const WriteAlbumContainer = () => {
   const [title, setTitle] = useState<string>('');
-  const [content, setContent] = useState<string>('');
-  const [IsOpen, setIsOpen] = useState<boolean>(true);
-  const [albumImg, setAlbumImg] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+  const [visible, setVisible] = useState<boolean>(true);
+  const [uploadImage, setUploadImage] = useState<string>('');
   const setIsUpload = useSetRecoilState(isUploadAtom);
+  const [albumImages, setAlbumImages] = useState<File | null>(null);
+  const emotionTagList = useRecoilValue(activeTagAtom);
 
   const handleTitleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setTitle(e.target.value);
   };
 
   const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setContent(e.target.value);
+    setDescription(e.target.value);
   };
 
   const handleIsOpen = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.target.value === 'open' ? setIsOpen(true) : setIsOpen(false);
+    e.target.value === 'open' ? setVisible(true) : setVisible(false);
+  };
+
+  const setImageFile = (file: File) => {
+    if (!file) {
+      alert('파일을 찾을 수 없습니다.');
+      return;
+    }
+    setAlbumImages(file);
+  };
+
+  const handleUpload = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const sendData = {
+      title,
+      description,
+      visible,
+      emotionTagList,
+    };
+
+    const res = postAlbum(sendData, albumImages);
+
+    if (!res) {
+      alert('앨범을 업로드했습니다.');
+    }
   };
 
   useEffect(() => {
@@ -33,14 +59,18 @@ const WriteAlbumContainer = () => {
 
   return (
     <WriteAlbumWrapper>
-      <ImageUpload albumImg={albumImg} setAlbumImg={setAlbumImg} />
+      <ImageUpload
+        uploadImage={uploadImage}
+        setUploadImage={setUploadImage}
+        setImageUpload={setImageFile}
+      />
       <WriteBox>
         <TitleForm title={title} handleTitleChange={handleTitleChange} />
-        <ContentForm content={content} handleContentChange={handleContentChange} />
+        <ContentForm description={description} handleContentChange={handleContentChange} />
         <EmotionForm />
-        <RadioForm IsOpen={IsOpen} handleIsOpen={handleIsOpen} />
+        <RadioForm visible={visible} handleIsOpen={handleIsOpen} />
       </WriteBox>
-      <IconButton width="5vw" height="30px" maxWidth="74px" minWidth="50px">
+      <IconButton width="5vw" height="30px" maxWidth="74px" minWidth="50px" onClick={handleUpload}>
         업로드
       </IconButton>
     </WriteAlbumWrapper>

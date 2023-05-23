acb08c1db9bf53e5b38c512fa47d2897c2e9d70b
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-05-23 14:01:16 +0900

    refactor : Album type, typecheck함수 분리

--- src/type/AlbumType.ts
+++ src/type/AlbumType.ts
@@ -1,7 +1,25 @@
-export type getAlbumContentType = {
-    albumId: number;
-    imageUrl: string;
-    commentCount: number;
-    empathyCount: number;
-    title: string;
-  };
+export type AlbumContent = {
+  albumId: number;
+  imageUrl: string;
+  commentCount: number;
+  empathyCount: number;
+  title: string;
+};
+
+export type AlbumDetail = {
+  changeable: boolean;
+  commentCount: number;
+  description: string;
+  emotionTagList: string[];
+  empathyCount: number;
+  imageUrlList: string[];
+  petName: string;
+  title: string;
+  visibility: 'PUBLIC' | 'PRIVATE';
+  writer: string;
+  writerProfileImageUrl: string;
+};
+
+export const isAlbumDetail = (arg: any): arg is AlbumDetail => {
+  return arg.imageUrlList !== undefined;
+};

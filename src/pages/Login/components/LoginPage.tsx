e02e879902752c4d3945798373f1f84246530e74
Jieun Lim <jieun776121@gmail.com>
2023-05-25 14:55:11 +0900

    feat : main merge

--- src/pages/Login/components/LoginPage.tsx
+++ src/pages/Login/components/LoginPage.tsx
@@ -40,48 +40,13 @@ const ButtonWrapper = styled.div`
   }
 `;
 const LoginPage = () => {
-<<<<<<< HEAD
-    const [showComponents, setShowComponents] = useState(false);
-
-    useEffect(() => {
-        setShowComponents(true);
-    }, []);
-
-    return (
-        <div>
-            {showComponents && (
-                <>
-                    <ContentWrapper>
-                        {/* <img src="/img/LoginBg.svg" /> */}
-                        <Image url={'/img/LoginBg.svg'} />
-                        <LoginWrapper>
-                            <div className='title'>소셜 로그인</div>
-                            <ButtonWrapper>
-
-                                <button><a href="http://52.78.181.46.nip.io/oauth2/authorization/google">
-                                    <img src="/img/GoogleLogin.svg" /></a>
-                                </button>
-
-                            </ButtonWrapper>
-                        </LoginWrapper>
-                    </ContentWrapper>
-                </>
-            )}
-        </div>
-    )
-}
-=======
   return (
     <div>
       <ContentWrapper>
-        {/* <img src="/img/LoginBg.svg" /> */}
         <Image url={'/img/LoginBg.jpg'} />
         <LoginWrapper>
           <div className="title">소셜 로그인</div>
           <ButtonWrapper>
-            <button>
-              <img src="/img/NaverLogin.svg" />
-            </button>
             <button>
               <a href="http://52.78.181.46.nip.io/oauth2/authorization/google">
                 <img src="/img/GoogleLogin.svg" />
@@ -93,5 +58,4 @@ const LoginPage = () => {
     </div>
   );
 };
->>>>>>> origin
 export default LoginPage;

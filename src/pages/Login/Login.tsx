c21fe15d49630d237b40083d8c208825e4320e0b
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-05-25 12:37:11 +0900

    fix : Login Page 이미지 파일수정

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,25 +1,14 @@
 import Navbar from '../../components/MainPageNavbar';
 import Footer from '../../components/Footer';
 import { LoginPage } from './components';
-import styled from 'styled-components';
 import { useEffect, useState } from 'react';
 
 const Login = () => {
-  const [showComponents, setShowComponents] = useState(false);
-
-  useEffect(() => {
-    setShowComponents(true);
-  }, []);
-
   return (
     <div>
-      {showComponents && (
-        <>
-          <Navbar key="navbar" />
-          <LoginPage key="loginPage" />
-          <Footer key="footer" />
-        </>
-      )}
+      <Navbar key="navbar" />
+      <LoginPage key="loginPage" />
+      <Footer key="footer" />
     </div>
   );
 };

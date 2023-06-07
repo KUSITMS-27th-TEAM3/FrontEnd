56c9c9585bff3b05b7ebc33422e73427179de9d9
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-06-07 11:56:59 +0900

    feat : React Query 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,32 @@ import './styles/fonts/font.css';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      retry: 0,
+      staleTime: 1000 * 60 * 5,
+      cacheTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <RecoilRoot>
         <ThemeProvider theme={theme}>
           <GlobalStyle />
           <Router />
         </ThemeProvider>
       </RecoilRoot>
-    </>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </QueryClientProvider>
   );
 }
 

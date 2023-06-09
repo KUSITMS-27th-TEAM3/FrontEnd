502c6babc577b7caf5b13de7a538731cdd153de1
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-06-09 14:26:43 +0900

    refactor : Radio Button 컴포넌트화

---
+++ src/components/common/RadioButton.tsx
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+const CheckBox = styled.input`
+  width: 30px;
+  height: 30px;
+  appearance: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  border: 2px solid #666666;
+  border-radius: 50%;
+  margin-right: 10px;
+  position: relative;
+  box-sizing: border-box;
+
+  &::before {
+    content: url('/img/check.svg');
+    position: absolute;
+    text-align: center;
+    line-height: 30px;
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  &:checked {
+    border: none;
+  }
+
+  &:checked::before {
+    content: url('/img/checkwhite.svg');
+    background-color: ${({ theme }) => theme.color.main.orange};
+    line-height: 34px;
+  }
+`;
+
+type RadioButtonProps = {
+  visible: boolean;
+  handleIsOpen: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  text: string;
+};
+
+const RadioButton = ({ visible, handleIsOpen, text }: RadioButtonProps) => {
+  return (
+    <>
+      <CheckBox type="radio" name="check2" value={text} onChange={handleIsOpen} checked={visible} />
+      <label htmlFor="open" className="write_content">
+        {text}
+      </label>
+    </>
+  );
+};
+
+export default RadioButton;
